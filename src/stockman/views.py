09645f738e795d72ff15eb5b0597ba04024abd6f
T-betrand <tebobertrand@gmail.com>
2021-08-13 11:33:25 +0100

    Stocks model,form, view, urls, templates

--- src/stockman/views.py
+++ src/stockman/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from .models import *
+
+
 # Create your views here.
+def home(request):
+    title = "Welcome to the home page"
+    form = "Welcome to the home page"
+    context = {
+        "title": title,
+        "test": form
+    }
+    return render(request, "home.html", context)
+
+
+def list_items(request):
+    title = "List of items"
+    queryset = Stock.objects.all()
+    context = {
+        "title": title,
+        "queryset": queryset,
+    }
+    return render(request, "list-item.html", context)
